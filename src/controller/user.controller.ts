700888ad6b708cf7399ca1ab36235cddd6b375db
Timos <psychoson91@gmail.com>
2026-04-19 19:33:36 +0300

    feat: add user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from '../services/user.service';
+import { UpdateUserDTO } from '../dto/user.dto';
+
+
+export const getOneByEmail = async (req:Request, res:Response, next: NextFunction) => {
+  try {
+    const email: string  = req.params.email;
+    const result  = await userService.findUserByEmail(email);
+    if (!result) 
+      return res.status(404).json({message: 'User not found by email'});
+    res.status(200).json(result);
+  } catch (err) {
+    next(err)
+  }
+}
 
 export const create = async(req:Request, res:Response, next: NextFunction) => {
   try {
@@ -9,4 +23,17 @@ export const create = async(req:Request, res:Response, next: NextFunction) => {
     console.log(err);
     next(err)
   }
+}
+
+export const update = async(req: Request, res: Response, next: NextFunction) => {
+  try{
+    const username: string =  req.params.username;
+    const data: UpdateUserDTO = req.body;
+    const result = await userService.updateUser(username, data);
+    if (!result)
+      return res.status(401).json({message: "User not found"})
+    res.status(200).json(result);
+  } catch (err) {
+    next(err)
+  } 
 }
