dbbf9db5895b0c49aa964472297bafa764209d63
Timos <psychoson91@gmail.com>
2026-04-21 17:20:13 +0300

    feat : user.controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,10 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 import * as userService from '../services/user.service';
 import { UpdateUserDTO } from '../dto/user.dto';
 
+export const getAll = async(req:Request, res:Response, next: NextFunction) => {
+  try{
+    const result = await userService.findUsers();
+    res.status(200).json(result);
+  }catch (err){
+    next(err)
+  }
+}
+
 
 export const getOneByEmail = async (req:Request, res:Response, next: NextFunction) => {
   try {
-    const email: string  = req.params.email;
+    const email: string  = req.params.email as string;
     const result  = await userService.findUserByEmail(email);
     if (!result) 
       return res.status(404).json({message: 'User not found by email'});
@@ -27,7 +36,7 @@ export const create = async(req:Request, res:Response, next: NextFunction) => {
 
 export const update = async(req: Request, res: Response, next: NextFunction) => {
   try{
-    const username: string =  req.params.username;
+    const username: string =  req.params.username as string;
     const data: UpdateUserDTO = req.body;
     const result = await userService.updateUser(username, data);
     if (!result)
